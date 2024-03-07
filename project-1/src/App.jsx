f0d5d4544627f371d3c49cde8dae22e128dd9ac6
Niki Kayastha <niki.kstha7664@gmail.com>
2024-03-07 23:19:16 +0545

    Adding Project 1

---
+++ project-1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+import Navigation from './componets/Navigation'
+import Hero from './componets/Hero'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
