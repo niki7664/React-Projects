8ced4b77e8bb9088b285da68a0b0607192c5713a
Niki Kayastha <niki.kstha7664@gmail.com>
2024-04-07 23:19:37 +0545

    Making small changes

--- project-2/src/components/Navigation/Navigation.jsx
+++ project-2/src/components/Navigation/Navigation.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
+import styles from "./Navigation.module.css"
 
 const Navigation = () => {
   return (
-    <nav className="container">
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo"/>
+    <nav className={`${styles.navigation} container`}>
+        <div className='logo'>
+            <img src="/images/Frame 2 1.png" alt="Logo"/> 
         </div>
 
-        <ul>
+        <ul >
           <li><a href="#">Home</a></li>
-          <li><a href="#">Location</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-
-        <button>Login</button>
-
-      
-
-      </nav>
+    </nav>
   )
 }
 
