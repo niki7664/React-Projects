b478b8dbd48e6fbd6646b9d2dbef63240a530950
Niki Kayastha <niki.kstha7664@gmail.com>
2024-04-07 23:17:29 +0545

    Making gemini clone

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SidePanel from './components/SidePanel/SidePanel'
+
+
+const App = () => {
+  return (
+    <>
+    <SidePanel/>
+    </>
+  )
+}
+
+export default App
