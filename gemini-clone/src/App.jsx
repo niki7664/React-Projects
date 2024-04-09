9674b1bfe7bb4c8062090efafee21bae3202703e
Niki Kayastha <niki.kstha7664@gmail.com>
2024-04-09 21:38:49 +0545

    Making gemini UI

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SidePanel from './components/SidePanel/SidePanel'
+import Main from './components/Main/Main'
 
 
 const App = () => {
   return (
     <>
     <SidePanel/>
+    <Main/>
     </>
   )
 }
--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import './Main.css'
+import {assets} from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt=""/>
+      </div>
+
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello,Niki.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <p>Settle a debate: how should you store bread?</p>
+            <img src={assets.bulb_icon} alt=""/>
+          </div>
+          <div className="card">
+            <p>Give me phrases to learn a new language</p>
+            <img src={assets.compass_icon} alt=""/>
+          </div>
+          <div className="card">
+            <p>Write a thank you note to my colleague</p>
+            <img src={assets.message_icon} alt=""/>
+          </div>
+          <div className="card">
+            <p>Give me phrases to learn a new language</p>
+            <img src={assets.code_icon} alt=""/>
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here"/>
+            <div>
+              <img src={assets.gallery_icon} alt=""/>
+              <img src={assets.mic_icon} alt=""/>
+              <img src={assets.send_icon} alt=""/>
+            </div>
+          </div>
+          <p className="bottom-info">
+          Gemini may display inaccurate info, including about people, so double-check its responses.<a href="https://support.google.com/gemini/answer/13594961?visit_id=638481591069937799-4079527195&p=privacy_notice&rd=1#privacy_notice">Your privacy and Gemini Apps</a>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
