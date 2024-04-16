0d06667121e00e4026550bb47c2f063b850c693c
Niki Kayastha <niki.kstha7664@gmail.com>
2024-04-16 18:33:27 +0545

    Adding generative AI functionality and making UI responsive

---
+++ gemini-clone/src/context/Context.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const onSent =async (prompt)=>{
+        await runChat(prompt)
+    }
+
+    onSent("what is react js")
+    const contextValue={
+
+    }
+
+    return(
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+
+    )
+}
